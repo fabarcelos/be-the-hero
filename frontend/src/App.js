57b8ebc010233d17d78ea8deb185b4d957098c1b
Fabricio Barcelos <fabriciobarcellos02@gmail.com>
2020-03-26 15:45:30 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './global.css';
+
+import Routes from './routes'
+// Usestate retorana um array de duas posiçoes:
+//[valor,Funçao de atualizaçao]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Fabigol
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
